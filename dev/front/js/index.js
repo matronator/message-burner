c810b086442491ddab33216499d7c0760a983327
Matronator <5470780+matronator@users.noreply.github.com>
2024-12-31 12:11:53 +0100

    downgrade build deps

--- dev/front/js/index.js
+++ dev/front/js/index.js
@@ -1,12 +1,13 @@
-import axette from "axette";
+import { Axette, noFlashUrl } from "axette";
 import "./import/darkmode.js";
 import { loadAds } from './import/load-ads.js';
 import "./import/popup.js";
 import "./../../../app/modules/Front/components/MessageForm/MessageForm.js"
 import { copyLink } from "./import/created.js";
 
+const axette = new Axette();
 axette.init();
-axette.fixURL();
+noFlashUrl();
 loadAds();
 
 const messageLink = document.getElementById(`messageLink`);
